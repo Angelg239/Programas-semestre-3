f88910176dedcf9f0fb3ae578c6a93495784e012
ANGEL\angel <angelaldrete239@gmail.com>
2025-09-10 08:32:28 -0600

    no message

--- prog03.py
+++ prog03.py
@@ -1,1 +1,10 @@
 
+# hacer un programa que lea 10 numeros y los almacene en un arreglo
+
+a = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+for i in range(10):
+    a[i] = int(input('Escribe un número \n'))
+
+for i in a:
+    print(i)
